c9175422d8aed07b17be179fdad54fcb7d5d4ef6
longdopphuocbao <86041237+longdopphuocbao@users.noreply.github.com>
2021-09-26 19:07:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hi Bảo Trâm!',
+    introTitle: 'Hi Phương Anh!',
     introDesc: `Hôm qua có ánh chiều tà
     Ngồi bên đồi núi bồi hồi nhớ em`,
     btnIntro: 'Next',
@@ -7,11 +7,11 @@ const CONFIG = {
     desc: 'Phải chăng em vẫn chờ anh',
     btnYes: 'Thích anh 🥰',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 9 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Next',
-    reply: 'Vì em là Bảo Trâm',
+    reply: 'Vì em là Phương Anh',
     mess: 'Anh biết mà. Yêu em nhiù nhìu',
     messDesc: 'Lo học mà thi thiệt tốt đi nha',
     btnAccept: 'Okeee lun',
-    messLink: 'https://www.facebook.com/BaoLong031203/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100004968302920' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
